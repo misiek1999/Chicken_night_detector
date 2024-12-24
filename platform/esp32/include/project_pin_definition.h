d0e5c8ce2083bd9a2c9b0287440cbd8250530865
misiek1999 <michaldydo99@gmail.com>
2024-12-24 12:32:24 +0100

    New esp32 pinout layout to use internal pullup resistors

--- platform/esp32/include/project_pin_definition.h
+++ platform/esp32/include/project_pin_definition.h
@@ -9,19 +9,19 @@ constexpr auto kPinMainDoorOutputUp = 17;
 constexpr auto kPinMainDoorOutputDown = 5;
 
 // control switches
-constexpr auto kPinEnableMainLight = 36;
-constexpr auto kPinEnableExternalLight = 39;
-constexpr auto kPinEnableAutoDoorControl = 34;
-constexpr auto kPinEnableManualDoorControl = 35;
-constexpr auto kPinControlDoorOpen = 32;
-constexpr auto kPinControlDoorClose = 33;
+constexpr auto kPinEnableMainLight = 14;
+constexpr auto kPinEnableExternalLight = 27;
+constexpr auto kPinEnableAutoDoorControl = 26;
+constexpr auto kPinEnableManualDoorControl = 25;
+constexpr auto kPinControlDoorOpen = 33;
+constexpr auto kPinControlDoorClose = 32;
 
 // control indicators
 constexpr auto kPinOnboardLed = 2;
 constexpr auto kPinMainLightIndicator = 19;
 constexpr auto kPinExternalLightIndicator = 18;
-constexpr auto kPinErrorIndicator = 25;
-constexpr auto kPinDoorMoveIndicator = 26;
+constexpr auto kPinErrorIndicator = 13;
+constexpr auto kPinDoorMoveIndicator = 12;
 
 // DS3231 RTC module
 constexpr auto kPinDS3231Clk = 22;
