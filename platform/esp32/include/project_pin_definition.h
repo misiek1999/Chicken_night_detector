4be07a83382a57bef1e11b5a059043271db0ebb4
misiek1999 <michaldydo99@gmail.com>
2024-12-29 00:23:13 +0100

    Fix problem with non booting esp.
    Selected pins cause problem with booting esp on target

--- platform/esp32/include/project_pin_definition.h
+++ platform/esp32/include/project_pin_definition.h
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 
 // actuators outputs
-constexpr auto kPinMainLigthOutput = 0;
+constexpr auto kPinMainLigthOutput = 16;
 constexpr auto kPinExternalLigthOutput = 4;
 constexpr auto kPinMainDoorOutputUp = 17;
 constexpr auto kPinMainDoorOutputDown = 5;
@@ -20,7 +20,7 @@ constexpr auto kPinOnboardLed = 2;
 constexpr auto kPinMainLightIndicator = 19;
 constexpr auto kPinExternalLightIndicator = 18;
 constexpr auto kPinErrorIndicator = 13;
-constexpr auto kPinDoorMoveIndicator = 12;
+constexpr auto kPinDoorMoveIndicator = 23;
 
 // DS3231 RTC module
 constexpr auto kPinDS3231Clk = 22;
