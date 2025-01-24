07dde7cf58f7ad822e22dee68e4735f7c5925f22
misiek1999 <michaldydo99@gmail.com>
2025-01-24 22:52:01 +0100

    Add debug logging for light state in LightSensorBulbController

--- src/light_sensor_bulb_controller.cpp
+++ src/light_sensor_bulb_controller.cpp
@@ -1,4 +1,5 @@
 #include "light_sensor_bulb_controller.h"
+#include "log.h"
 #include <Arduino.h>
 
 ControlLogic::LightSensorBulbController::LightSensorBulbController(lightStateCallback light_sensor_callback):
@@ -15,6 +16,7 @@ bool ControlLogic::LightSensorBulbController::periodicUpdateController() {
     if (current_time - last_light_change_time_ > kDelayToDisableLightMs) {
         light_state_ = LightState::Off;
     }
+    LOG_DEBUG("Light state: %d", static_cast<int>(light_state_));
     return true;
 }
 
