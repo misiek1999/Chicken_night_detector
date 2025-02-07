361d065763f43d7933c575527dcce2b5651aa6f8
misiek1999 <michaldydo99@gmail.com>
2025-02-07 23:50:55 +0100

    Add verbose logging for light sensor output and debug log for light disable condition

--- src/light_sensor_bulb_controller.cpp
+++ src/light_sensor_bulb_controller.cpp
@@ -9,12 +9,15 @@ ControlLogic::LightSensorBulbController::LightSensorBulbController(lightStateCal
 bool ControlLogic::LightSensorBulbController::periodicUpdateController() {
     const auto current_time = millis();
     const auto current_light_state = light_sensor_callback_() ? LightState::On : LightState::Off;
+    LOG_VERBOSE("Light sensor output: %d", static_cast<int>(current_light_state));
     if (current_light_state != light_state_) {
         light_state_ = current_light_state;
         last_light_change_time_ = current_time;
     }
     if (current_time - last_light_change_time_ > kDelayToDisableLightMs) {
         light_state_ = LightState::Off;
+        LOG_DEBUG("Exceeded active time, disable light (curr) %d, (last) %d",
+                   static_cast<int>(current_time), static_cast<int>(last_light_change_time_));
     }
     LOG_DEBUG("Light state: %d", static_cast<int>(light_state_));
     return true;
